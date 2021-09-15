2681ecff123f42f9a4ecf7a1e9e75fc89e6ecc7d
LiSoulSpace <56160001+LiSoulSpace@users.noreply.github.com>
2021-09-15 10:46:49 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    val kotlinVersion = "1.5.30"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("net.mamoe.mirai-console") version "2.7.1-dev-1"
+}
+
+group = "com.github.soulspace"
+version = "0.1.0"
+
+repositories {
+    mavenLocal()
+    maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
+    mavenCentral()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    }
+}
+// TODO changed the name to your plugin's name
+rootProject.name = "mirai-console-plugin-template"
